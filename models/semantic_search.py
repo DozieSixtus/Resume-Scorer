34c0f7b89b62436514b9e577d2fdd83a985b86a3
Chiedozie Onyearugbulem <doziesixtus@gmail.com>
2023-05-21 20:33:44 +0100

    trained resume scorer

--- models/semantic_search.py
+++ models/semantic_search.py
@@ -3,7 +3,7 @@ import os, pandas as pd, torch
 import sys
 from transformers import AutoTokenizer, TFAutoModel
 sys.path.append(os.getcwd() + '\\scripts')
-import prep_csv
+import prep_csv, job_desc
 
 dataset = dict()
 dataset['cvContent'] = prep_csv.partitionText()[:200]
@@ -29,8 +29,29 @@ embedding = get_embeddings(dataset['cvContent'][0])
 print(embedding.shape)
 
 embeddings_dataset = dataset.map(
-    lambda x: {"embeddings": get_embeddings(x["cvContent"]).numpy()[0]}, batched=True
+    lambda x: {"embeddings": get_embeddings(x["cvContent"]).numpy()[0]}
 )
 print(embeddings_dataset)
 
 embeddings_dataset.add_faiss_index(column="embeddings")
+
+def queryTexts():
+    jobDesc = job_desc.loadJobDesc()
+    for query in jobDesc:
+        queryEmbedding = get_embeddings(query).numpy()
+        print(queryEmbedding.shape)
+        scores, samples = embeddings_dataset.get_nearest_examples(
+            "embeddings", queryEmbedding, k=3
+        )
+        samplesDf = pd.DataFrame.from_dict(samples)
+        samplesDf["scores"] = scores
+        samplesDf.sort_values("scores", ascending=False, inplace=True)
+        
+        for _, row in samplesDf.iterrows():
+            print(f"COMMENT: {row.cvContent}")
+            print(f"SCORE: {row.scores}")
+            print("="*50)
+            print()
+            
+if __name__ == '__main__':
+    queryTexts()
--- scripts/job_desc.py
+++ scripts/job_desc.py
@@ -5,7 +5,9 @@ def loadJobDesc():
     desc = ''.join(x if ord(str(x))<128 else '' for x in desc)
     desc = desc.splitlines()
     desc = [re.split('\. ',x) for x in desc]
-    print(desc)
+    desc = [[x] for y in desc for x in y]
+    return desc
+
 
 if __name__ == '__main__':
     loadJobDesc()
