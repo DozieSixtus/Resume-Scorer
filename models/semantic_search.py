8446fd420cf6657d977c643141575eaa7cdc2970
Chiedozie Onyearugbulem <doziesixtus@gmail.com>
2023-05-26 12:34:48 +0100

    computed scores

--- models/semantic_search.py
+++ models/semantic_search.py
@@ -26,7 +26,6 @@ def get_embeddings(text_list):
     return cls_pooling(model_output)
 
 embedding = get_embeddings(dataset['cvContent'][0])
-print(embedding.shape)
 
 embeddings_dataset = dataset.map(
     lambda x: {"embeddings": get_embeddings(x["cvContent"]).numpy()[0]}
@@ -36,22 +35,27 @@ print(embeddings_dataset)
 embeddings_dataset.add_faiss_index(column="embeddings")
 
 def queryTexts():
+    jobScores = embeddings_dataset.to_pandas()
     jobDesc = job_desc.loadJobDesc()
-    for query in jobDesc:
+    for i,query in enumerate(jobDesc):
         queryEmbedding = get_embeddings(query).numpy()
         print(queryEmbedding.shape)
         scores, samples = embeddings_dataset.get_nearest_examples(
-            "embeddings", queryEmbedding, k=3
+            "embeddings", queryEmbedding, k=len(embeddings_dataset)
         )
         samplesDf = pd.DataFrame.from_dict(samples)
-        samplesDf["scores"] = scores
-        samplesDf.sort_values("scores", ascending=False, inplace=True)
+        #print(samplesDf.head(5))
+        samplesDf[f"scores{i}"] = scores
+        jobScores = jobScores.merge(samplesDf[['cvContent',f"scores{i}"]], on='cvContent', how='left')
+        samplesDf.sort_values(f"scores{i}", ascending=False, inplace=True)
+        #print(jobScores)
         
         for _, row in samplesDf.iterrows():
-            print(f"COMMENT: {row.cvContent}")
-            print(f"SCORE: {row.scores}")
-            print("="*50)
+            #print(f"COMMENT: {row.cvContent}")
+            #print(f"SCORE: {row.scores}")
+            #print("="*50)
             print()
+    jobScores.to_csv(os.getcwd()+'\\models\\jobScores.csv')
             
 if __name__ == '__main__':
     queryTexts()
